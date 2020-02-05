523411252ead0b8c4ffbdc4b536d32985c05d67b
Maroti Ganeshrao Mungal <maroti.mungal@yash.com>
2020-02-05 09:26:58 +0530

    added mysql depadancy for DB connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { databaseProviders } from './../database.providers';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,...databaseProviders],
+  exports: [...databaseProviders]
 })
 export class AppModule {}
