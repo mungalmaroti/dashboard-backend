da4c841c92e2f84eae8f4cab5cb17d1471169fc2
unknown <mungalmaroti@git.com>
2020-02-23 18:03:36 +0530

    [Auth Module] :Token Base Authentication implemention done

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,16 @@
 import { Module } from '@nestjs/common';
-//import { AppController } from './app.controller';
+import { AppController } from './app.controller';
 //import { AppService } from './app.service';
 //import { databaseProviders } from './../database.providers';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Connection } from 'typeorm';
 import { EmployeeModule } from './employee/employee.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(),UsersModule, EmployeeModule],
-  controllers: [],
+  imports: [TypeOrmModule.forRoot(),UsersModule, EmployeeModule,AuthModule],
+  controllers: [AppController],
   providers: [],
   exports: []
 })
